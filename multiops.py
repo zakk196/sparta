bf12e72afcfbc4b67ec0c5f7f5b9e17793411b20
zak <zkhatri@spartaglobal.com>
2021-09-06 21:12:25 +0100

    multiops_python

---
+++ multiops.py
@@ -0,0 +1,15 @@
+first_num = input("enter a number:           ")
+second_num = input("enter a second number:    ")
+
+first_num = int(first_num)
+second_num= int(second_num)
+
+a = first_num + second_num
+b =first_num - second_num
+c = first_num * second_num
+d = first_num / second_num
+
+print(first_num, "+", second_num, "=", a)
+print(first_num, "-", second_num, "=", b)
+print(first_num, "*", second_num, "=", c)
+print(first_num, "/", second_num, "=", d)
